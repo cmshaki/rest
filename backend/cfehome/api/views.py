30ceb1a0b49bdbabe04eb74dd21c1d00bf9b9541
Cris Shaki <cmshaki@gmail.com>
2023-11-13 15:36:53 +0300

    added serializer and rest_framework to installed apps

--- backend/cfehome/api/views.py
+++ backend/cfehome/api/views.py
@@ -1,15 +1,21 @@
-import json
-from django.http import JsonResponse
+from django.forms.models import model_to_dict
+
+# from django.http import JsonResponse, HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from products.models import Product
+from products.serializers import ProductSerializer
 
 
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
-    model_data = Product.objects.all().order_by("?").first()
+    """DRF API VIEW"""
+    instance = Product.objects.all().order_by("?").first()
     data = {}
-    if model_data:
-        data["id"] = model_data.id
-        data["title"] = model_data.title
-        data["content"] = model_data.content
-        data["price"] = model_data.price
-    return JsonResponse(data)
+    if instance:
+        # data = model_to_dict(
+        #     instance, fields=["id", "title", "price", "sale_price", "get_discount"]
+        # )
+        data = ProductSerializer(instance).data
+    return Response(data)
